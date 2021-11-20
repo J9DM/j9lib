04b8d5f2b2332d3fd3192d8d421243f1b5d7bd6a
Jennine Dawicki-McKenna <jdmckenna00@gmail.com>
2021-11-20 08:36:30 -0500

    Removed txdb and commented out @examples. Devtools check completes without errors/warnings/notes.

--- R/txdb.R
+++
@@ -1,6 +0,0 @@
-#' TxDb object.
-#'
-#' A TxDb object from TxDb.Hsapiens.UCSC.hg38.knownGene.
-#'
-#' @format A TxDb object.
-"txdb"
--- data-raw/txdb.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `txdb` dataset goes here
-
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene
-
-usethis::use_data(txdb, overwrite = TRUE)
